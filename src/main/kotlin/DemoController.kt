8d5c1919621bd9bad4f88ce498250e110ba8ddec
Peter Lycos <plycos@gmail.com>
2026-08-12 10:24:36 -0400

    run linter

--- src/main/kotlin/DemoController.kt
+++ src/main/kotlin/DemoController.kt
@@ -9,16 +9,18 @@ import jakarta.ws.rs.core.MediaType
 
 @Path("/demo")
 class DemoController(
-    val demo: Template
+    val demo: Template,
 ) {
     @GET
     @Produces(MediaType.TEXT_HTML)
-    fun index(@QueryParam("name") @DefaultValue("World") name: String): TemplateInstance =
-        demo.data("name", name)
+    fun index(
+        @QueryParam("name") @DefaultValue("World") name: String,
+    ): TemplateInstance = demo.data("name", name)
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/json")
-fun json(@QueryParam("name") @DefaultValue("World") name: String): DemoModel =
-        DemoModel(name)
+    fun json(
+        @QueryParam("name") @DefaultValue("World") name: String,
+    ): DemoModel = DemoModel(name)
 }
--- src/main/kotlin/DemoModel.kt
+++ src/main/kotlin/DemoModel.kt
@@ -3,5 +3,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class DemoModel(
     val name: String,
-    val timestamp: Long = System.currentTimeMillis()
+    val timestamp: Long = System.currentTimeMillis(),
 )
